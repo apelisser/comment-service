e5d8132cd575bebd9cc1076f3d99592e7da96110
Abner J Pelisser <abner.pelisser@gmail.com>
2025-05-18 21:32:17 -0300

    chore: set timezone utc as default

--- src/main/java/com/apelisser/algacomments/commentservice/CommentServiceApplication.java
+++ src/main/java/com/apelisser/algacomments/commentservice/CommentServiceApplication.java
@@ -3,10 +3,15 @@ package com.apelisser.algacomments.commentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
+import static java.time.ZoneOffset.UTC;
+
 @SpringBootApplication
 public class CommentServiceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(UTC));
 		SpringApplication.run(CommentServiceApplication.class, args);
 	}
 
